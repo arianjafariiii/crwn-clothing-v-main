018d62c8ac1bfc8b657bd142f3d61c6b53af6fa2
ArianJafari <arian.jafari2233@gmail.com>
2025-10-17 16:22:14 +0330

    program section added to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from './components/navbar/navbar';
 import Hero from './components/hero/hero';
+import Programs from './components/programs/programs';
+import Title from './components/title/title';
 
 
 const App = () => {
@@ -8,6 +10,11 @@ const App = () => {
     <div>
       <Navbar/>
       <Hero/>
+      <div className="container">
+        <Title subtitle='Our PROGRAM' title="What We Offer" />
+        <Programs/>
+      </div>
+      
     </div>
   )
 }
