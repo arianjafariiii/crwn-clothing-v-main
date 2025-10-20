5db30763822f8a382b80dbf52b763904253daa03
ArianJafari <arian.jafari2233@gmail.com>
2025-10-20 14:29:40 +0330

    Resolved merge conflicts after pulling from remote

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from "./assets/logo.png";
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src={logo} alt="nav-logo" />
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+  )
+}
+export default Navbar
